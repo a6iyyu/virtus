423f3db035d72979ceae3049f087b3e5b34ecf0b
a6iyyu <ffst4irs@gmail.com>
2023-12-10 20:58:32 +0700

    Add Top Trending Artists

--- static/js/homepage.js
+++ static/js/homepage.js
@@ -1,10 +1,16 @@
-var splide = new Splide(".splide", {
+const splide1 = new Splide(".splide__1", {
   type: "loop",
   drag: "free",
   snap: true,
   perPage: 3,
 });
-splide.mount();
+splide1.mount();
+
+var splide2 = new Splide(".splide__2", {
+  type: "loop",
+  padding: "5rem",
+});
+splide2.mount();
 
 const FindMore = document.querySelector(".find-more");
 const FinishToScroll = document.querySelector(".top-trending-parent");
