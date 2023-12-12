d34cf5b69448ac75298b2f8f41553798a88d7222
a6iyyu <ffst4irs@gmail.com>
2023-12-12 15:37:45 +0700

    Add menu feature in landing page

--- static/js/homepage.js
+++ static/js/homepage.js
@@ -1,4 +1,13 @@
-const splide1 = new Splide(".splide__1", {
+const splide = new Splide(".splide__1", {
+  type: 'slide',
+  perPage: 5,
+  focus: 0,
+  omitEnd: true,
+  arrows: false,
+});
+splide.mount();
+
+const splide1 = new Splide(".splide__2", {
   type: "loop",
   drag: "free",
   snap: true,
@@ -6,7 +15,7 @@ const splide1 = new Splide(".splide__1", {
 });
 splide1.mount();
 
-var splide2 = new Splide(".splide__2", {
+const splide2 = new Splide(".splide__3", {
   type: "loop",
   padding: "5rem",
 });
