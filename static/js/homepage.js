d02826d3d3483efc2352ebb7d6b8af43c2eecd40
a6iyyu <ffst4irs@gmail.com>
2023-12-15 15:13:03 +0700

    P

--- static/js/homepage.js
+++ static/js/homepage.js
@@ -1,42 +1,59 @@
 document.addEventListener("DOMContentLoaded", function () {
   const splide__1 = new Splide(".splide__1", {
-    type: "slide",
-    perPage: 5,
+    arrows: false,
     focus: 0,
     omitEnd: true,
-    arrows: false,
+    perPage: 5,
+    type: "slide",
   });
   splide__1.mount();
 });
 
 document.addEventListener("DOMContentLoaded", function () {
   const splide__2 = new Splide(".splide__2", {
-    type: "loop",
     drag: "free",
-    snap: true,
     perPage: 3,
+    snap: true,
+    type: "loop",
   });
   splide__2.mount();
 });
 
 document.addEventListener("DOMContentLoaded", function () {
   const splide__3 = new Splide(".splide__3", {
-    type: "loop",
     drag: "free",
-    snap: true,
     perPage: 3,
+    snap: true,
+    type: "loop",
   });
   splide__3.mount();
 });
 
 document.addEventListener("DOMContentLoaded", function () {
   const splide__4 = new Splide(".splide__4", {
-    type: "loop",
+    arrows: false,
     direction: "ttb",
     height: "10rem",
-    arrows: false,
+    type: "loop",
   });
   splide__4.mount();
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  new Splide(".splide__5", {
+    arrows: true,
+    cover: true,
+    drag: "free",
+    gap: "1rem",
+    heightRatio: 0.3,
+    updateOnMove: true,
+    perPage: 1,
+    wheel: true,
+    width: "90%",
+    video: {
+      loop: true,
+    },
+  }).mount(window.splide.Extensions);
 });
 
 const FindMore = document.querySelector(".find-more");
