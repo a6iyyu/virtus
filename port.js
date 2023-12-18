5427d33131aa3fc4cbf9266ea65a6cfcb71470d9
a6iyyu <ffst4irs@gmail.com>
2023-12-18 22:02:13 +0700

    Added several pages and finalized the footer.

--- port.js
+++ port.js
@@ -20,12 +20,52 @@ app.get("/about", async (request, response) => {
   response.render("about");
 });
 
+app.get("/analytics", async (request, response) => {
+  response.render("analytics");
+});
+
+app.get("/blog", async (request, response) => {
+  response.render("blog");
+});
+
 app.get("/careers", async (request, response) => {
   response.render("careers");
 });
 
 app.get("/community", async (request, response) => {
   response.render("community");
+});
+
+app.get("/explore", async (request, response) => {
+  response.render("explore");
+});
+
+app.get("/faq", async (request, response) => {
+  response.render("faq");
+});
+
+app.get("/help-center", async (request, response) => {
+  response.render("help-center");
+});
+
+app.get("/limited-editions", async (request, response) => {
+  response.render("limited-editions");
+});
+
+app.get("/profile", async (request, response) => {
+  response.render("profile");
+});
+
+app.get("/saved", async (request, response) => {
+  response.render("saved");
+});
+
+app.get("/services", async (request, response) => {
+  response.render("services");
+});
+
+app.get("/terms-and-privacy", async (request, response) => {
+  response.render("terms-and-privacy");
 });
 
 app.use("/", async (request, response) => {
