debf54cc444bf6fda1fabfddf4ce175bb40c837c
a6iyyu <ffst4irs@gmail.com>
2023-12-26 16:15:16 +0700

    Add a blog slider to the blog page.

---
+++ static/js/blog.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-undef */
+document.addEventListener("DOMContentLoaded", function () {
+  const splide__1 = new Splide(".splide__1", {
+    arrows: false,
+    autoplay: true,
+    drag: "free",
+    pagination: false,
+    perPage: 2,
+    snap: true,
+    type: "loop",
+  });
+  splide__1.mount();
+});
