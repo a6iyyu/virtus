baa11ab8f8c84686c683bbf113c7ea26d9b81d34
a6iyyu <ffst4irs@gmail.com>
2023-12-28 15:05:52 +0700

    🌐 Add some effects.

--- static/js/careers.js
+++ static/js/careers.js
@@ -7,3 +7,14 @@ document.addEventListener("DOMContentLoaded", function () {
     type: "loop",
   }).mount(window.splide.Extensions);
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const splide__2 = new Splide(".splide__2", {
+    arrows: false,
+    focus: 0,
+    omitEnd: true,
+    perPage: 5,
+    type: "loop",
+  });
+  splide__2.mount();
+});
