09a128f3bc768b648c7c7f23ace1c8b12d6bf655
a6iyyu <ffst4irs@gmail.com>
2024-01-06 17:10:31 +0700

    Fixed little things.

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,13 @@
 import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
 
 export default defineConfig({
-  base: "/Virtus/",
-  plugins: [vue()],
+  plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
-      "@": fileURLToPath(new URL("./client", import.meta.url)),
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
 });
